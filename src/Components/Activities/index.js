81ed4ac457de37b4748dc250248c37d48e5e62f8
SantiH97 <santiago.herrera1224@gmail.com>
2023-05-26 18:50:18 -0300

    MR-40: Add Add functionality, have it mostly functioning

--- src/Components/Activities/index.js
+++ src/Components/Activities/index.js
@@ -1,9 +1,76 @@
+import { useEffect, useState } from 'react';
 import styles from './activities.module.css';
 
 function Activities() {
+  const [activities, setActivities] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const onClick = (e) => {
+    e.preventDefault();
+    fetch(`${process.env.REACT_APP_API}/api/activities/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        isActive: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log(response.body);
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response.body);
+        setActivities(response);
+        setName('');
+        setDescription('');
+      });
+  };
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/api/activities/`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setActivities(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Activities</h2>
+      <div>
+        {activities.map((activity) => {
+          return (
+            <h3 key={activity._id}>
+              {activity.name}: {activity.description}
+            </h3>
+          );
+        })}
+      </div>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Activity name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <textarea
+          type="text"
+          placeholder="Activity description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <button onClick={onClick} type="submit">
+          Add
+        </button>
+      </form>
     </section>
   );
 }
