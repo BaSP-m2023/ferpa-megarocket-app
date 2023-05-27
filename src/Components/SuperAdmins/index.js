39bb3b0031cc9988551ad86203eb7bb10a879279
Daniel Lezama <dlezama0796@gmail.com>
2023-05-27 18:41:18 -0300

    MR-46: super admin table

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,9 +1,41 @@
 import styles from './super-admins.module.css';
 
+const data = [
+  { email: 'asdfasdf@gmail.com', password: '1234' },
+  { email: 'ghlkhgfnh@hotmail.com', password: '324235' }
+];
+
 function SuperAdmins() {
   return (
     <section className={styles.container}>
-      <h2>SuperAdmins</h2>
+      <div className={styles.box}>
+        <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Password</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.email}</td>
+                  <td className={styles.pass}>{item.password}</td>
+                  <td>
+                    <button>Edit</button>
+                  </td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
