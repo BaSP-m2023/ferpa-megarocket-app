6d5ee55d6d5601ee81f3d9f6d3bd794ef21acb0d
Juan Sobral <JuanignacioSobral@gmail.com>
2023-05-30 23:04:55 -0300

    MR-42: created the PUT feature

--- src/Components/Classes/Table/Table.js
+++ src/Components/Classes/Table/Table.js
@@ -1,35 +1,127 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Table = ({ data, deleteItem }) => {
+const Table = ({ data, deleteItem, onEditItem, activities, trainers }) => {
+  const [klass, setKlass] = useState({
+    _id: '',
+    day: '',
+    hour: '',
+    activityId: '',
+    trainerId: '',
+    slots: ''
+  });
+
+  const onChangeInput = (e) => {
+    setKlass({
+      ...klass,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const funcionCargaDatos = (item) => {
+    setKlass({
+      _id: item._id,
+      day: item.day,
+      hour: item.hour,
+      activityId: item.activityId,
+      trainerId: item.trainerId,
+      slots: item.slots
+    });
+    console.log(item);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onEditItem(klass._id, klass);
+    console.log(klass);
+    setKlass({
+      day: '',
+      hour: '',
+      activityId: '',
+      trainerId: '',
+      slots: ''
+    });
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Activity Name</th>
-          <th>day</th>
-          <th>hour</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => {
-          return (
-            <tr key={item._id}>
-              {item.activityId.name ? <td>{item.activityId.name}</td> : <td>Undefined</td>}
-              <td>{item.day}</td>
-              <td>{item.hour}</td>
-              <td></td>
-              <td>
-                <button className="deleteButton" onClick={() => deleteItem(item._id)}>
-                  X
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Activity Name</th>
+            <th>day</th>
+            <th>hour</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item._id}>
+                {item.activityId.name ? <td>{item.activityId.name}</td> : <td>Undefined</td>}
+                <td>{item.day}</td>
+                <td>{item.hour}</td>
+                <td>{item.slots}</td>
+                <td></td>
+                <td>
+                  <button className="editButton" onClick={() => funcionCargaDatos(item)}>
+                    Edit Item
+                  </button>
+                </td>
+                <td>
+                  <button className="deleteButton" onClick={() => deleteItem(item._id)}>
+                    X
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <form onSubmit={onSubmit}>
+        <h3> Edit Form! </h3>
+        <div>
+          <div className="input container">
+            <label>Day</label>
+            <input name="day" type="text" value={klass.day} onChange={onChangeInput} />
+          </div>
+          <div className="input container">
+            <label>Hour</label>
+            <input name="hour" type="number" value={klass.hour} onChange={onChangeInput} />
+          </div>
+        </div>
+        <div className="input container">
+          <label>Activity Name</label>
+          <select name="activityId" value={klass.activityId} onChange={onChangeInput}>
+            {activities.map((activity) => {
+              return (
+                <option key={activity._id} value={activity._id}>
+                  {activity.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="input container">
+          <label>Trainer</label>
+          <select name="trainerId" value={klass.trainerId} onChange={onChangeInput}>
+            {trainers.map((trainer) => {
+              return (
+                <option key={trainer._id} value={trainer._id}>
+                  {trainer.firstName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="input container">
+          <label>Slots</label>
+          <input name="slots" type="number" value={klass.slots} onChange={onChangeInput} />
+        </div>
+        <button type="submit">Update</button>
+      </form>
+    </div>
   );
 };
 
--- src/Components/Classes/index.js
+++ src/Components/Classes/index.js
@@ -14,12 +14,6 @@ const Classes = () => {
     setClasses(data.data);
   };
 
-  // const getClass = async (_id) => {
-  //   const response = await fetch(`${process.env.REACT_APP_API}/api/classes/${_id}`);
-  //   const data = await response.json();
-  //   return data;
-  // };
-
   const getActivities = async () => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/activities/`);
     const data = await response.json();
@@ -59,23 +53,6 @@ const Classes = () => {
     }
   };
 
-  // const response = await fetch(`${process.env.REACT_APP_API}/api/classes/`, {
-  //   method: 'POST',
-  //   Headers: {
-  //     'Content-type': 'application/json'
-  //   },
-  //   body: JSON.stringify({ activityId, day, hour })
-  // });
-  // const data = await response.json();
-  // const newItem = {
-  //   _id: (Math.floor = Math.random() * 1000),
-  //   activityId: { name: activityId },
-  //   day,
-  //   hour
-  // };
-  //   setClasses([...classes, data]);
-  // };
-
   const deleteItem = async (_id) => {
     await fetch(`${process.env.REACT_APP_API_URL}/api/classes/${_id}`, {
       method: 'DELETE'
@@ -83,30 +60,40 @@ const Classes = () => {
     setClasses([...classes.filter((klass) => klass._id !== _id)]);
   };
 
-  // const updateItem = async (_id) => {
-  //   const classToUpdate = await getClass(_id);
-  //   const updatedClass = {
-  //     ...classToUpdate,
-  //     day: data.day,
-  //     hour: data.hour,
-  //     slots: data.slots,
-  //     activityId: { name: data.activityId.name }
-  //   };
+  const updateClass = async (id, updatedClass) => {
+    const adminIndex = classes.findIndex((klass) => klass._id === id);
+    try {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/classes/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedClass)
+      });
 
-  //   const response = await fetch(`${process.env.REACT_APP_API}/api/classes/`, {
-  //     method: 'PUT',
-  //     Headers: {
-  //       'Content-type': 'application/json'
-  //     },
-  //     body: JSON.stringify(updatedClass)
-  //   });
-  //   const data = await response.json();
-  // };
+      const { message, data, error } = await res.json();
+      alert(message);
+
+      if (!error) {
+        const actualClasses = [...classes];
+        actualClasses[adminIndex] = data;
+        setClasses(actualClasses);
+      }
+    } catch (error) {
+      console.error;
+    }
+  };
 
   return (
     <section className={styles.container}>
       <h2>Classes</h2>
-      <Table data={classes} deleteItem={deleteItem} />
+      <Table
+        data={classes}
+        deleteItem={deleteItem}
+        onEditItem={updateClass}
+        activities={activities}
+        trainers={trainers}
+      />
       <Form onAddItem={addItem} activities={activities} trainers={trainers} />
     </section>
   );
