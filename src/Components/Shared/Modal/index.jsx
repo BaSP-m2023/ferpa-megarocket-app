03e859328ed13a79b37c349094f8aedf200b867f
Daniel Lezama <dlezama0796@gmail.com>
2023-06-03 20:14:32 -0300

    MR-55: added a modal for success

--- src/Components/Shared/Modal/index.jsx
+++ src/Components/Shared/Modal/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from './modal.module.css';
 
-const Modal = ({ isOpen, onClose, title, children }) => {
+const Modal = ({ isOpen, onClose, title, children, success }) => {
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContent}>
+    <div className={success ? styles.modalOverlaySuccess : styles.modalOverlay}>
+      <div className={`${styles.modalContent} ${success ? styles.modalSuccess : ''}`}>
         <div className={styles.modalHeader}>
           <h2 className={styles.modalTitle}>{title}</h2>
           <span className={styles.modalCloseBtn} onClick={() => onClose()}>
             &times;
           </span>
         </div>
-        {children}
+        <div className={styles.modalChildren}>{children}</div>
       </div>
     </div>
   );
