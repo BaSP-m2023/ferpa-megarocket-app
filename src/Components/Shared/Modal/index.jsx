035d9edf807159c5a9daede28d046dca892048c3
Daniel Lezama <dlezama0796@gmail.com>
2023-06-04 13:01:37 -0300

    MR-71: added props (text and warning)

--- src/Components/Shared/Modal/index.jsx
+++ src/Components/Shared/Modal/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './modal.module.css';
 
-const Modal = ({ isOpen, onClose, title, children, success }) => {
+const Modal = ({ isOpen, onClose, title, text, children, success, warning }) => {
   if (!isOpen) {
     return null;
   }
@@ -10,11 +10,12 @@ const Modal = ({ isOpen, onClose, title, children, success }) => {
     <div className={success ? styles.modalOverlaySuccess : styles.modalOverlay}>
       <div className={`${styles.modalContent} ${success ? styles.modalSuccess : ''}`}>
         <div className={styles.modalHeader}>
-          <h2 className={styles.modalTitle}>{title}</h2>
+          <h2 className={`${styles.modalTitle} ${warning ? styles.modalWarn : ''}`}>{title}</h2>
           <span className={styles.modalCloseBtn} onClick={() => onClose()}>
             &times;
           </span>
         </div>
+        <p className={styles.modalText}>{text}</p>
         <div className={styles.modalChildren}>{children}</div>
       </div>
     </div>
