403a67e9e77e9408b78685fcd1796bc2f1d36af3
Juan Sobral <JuanignacioSobral@gmail.com>
2023-06-04 21:47:45 -0300

    MR-51: Swaped switch logic for variable as indicated on the comments

--- src/Components/Shared/Button/index.jsx
+++ src/Components/Shared/Button/index.jsx
@@ -2,26 +2,7 @@ import React from 'react';
 import styles from './button.module.css';
 
 const Button = ({ text, clickAction, type, disabled }) => {
-  let classNameStyle;
-  switch (type) {
-    case 'delete':
-      classNameStyle = styles.delete;
-      break;
-    case 'deleteIcon':
-      classNameStyle = styles.deleteIcon;
-      break;
-    case 'seePassword':
-      classNameStyle = styles.seePassword;
-      break;
-    case 'edit':
-      classNameStyle = styles.edit;
-      break;
-    case 'add':
-      classNameStyle = styles.add;
-      break;
-    default:
-      classNameStyle = styles.white;
-  }
+  let classNameStyle = styles[type];
   return (
     <button onClick={clickAction} className={`${styles.btn} ${classNameStyle}`} disabled={disabled}>
       {text}
