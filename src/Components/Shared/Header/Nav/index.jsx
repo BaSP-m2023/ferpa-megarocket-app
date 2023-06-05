73f77fcf15c77d1aca1a3476ac17d187c39e18c6
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-06-05 13:55:21 -0300

    MR-76: NavLink activeClassName classes fixed

--- src/Components/Shared/Header/Nav/index.jsx
+++ src/Components/Shared/Header/Nav/index.jsx
@@ -6,60 +6,28 @@ const Nav = () => {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.links}>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={styles.link}
-          to="/home"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/home">
           <li>Home</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={styles.link}
-          to="/activities"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/activities">
           <li>Activities</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/admins"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/admins">
           <li>Admins</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/classes"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/classes">
           <li>Classes</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/members"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/members">
           <li>Members</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/subscriptions"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/subscriptions">
           <li>Subscriptions</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/super-admins"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/super-admins">
           <li>Super Admins</li>
         </NavLink>
-        <NavLink
-          activeClassName={`${styles.link} ${styles.linkFocused}`}
-          className={`${styles.link}`}
-          to="/trainers"
-        >
+        <NavLink activeClassName={styles.linkFocused} className={styles.link} to="/trainers">
           <li>Trainers</li>
         </NavLink>
       </ul>
