dd875fffc129ae209cecf76367740a86415d7c2c
SantiH97 <santiago.herrera1224@gmail.com>
2023-06-05 17:15:05 -0300

    Set-up/MR-78/Fix index file names

--- src/Components/Shared/Footer/Index.jsx
+++ src/Components/Shared/Footer/Index.jsx
@@ -8,21 +8,21 @@ function Footer() {
         <div>
           <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
             <img
-              alt={'twitter'}
+              alt={'Twitter'}
               className={styles.socialIcon}
               src={'./assets/images/Twitter-Icon.svg'}
             />
           </a>
           <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
             <img
-              alt={'instagram'}
+              alt={'Instagram'}
               className={styles.socialIcon}
               src={'./assets/images/Instagram-Icon.svg'}
             />
           </a>
           <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
             <img
-              alt={'facebook'}
+              alt={'Facebook'}
               className={styles.socialIcon}
               src={'./assets/images/Facebook-Logo.svg'}
             />
--- src/Components/Shared/Header/Index.jsx
+++ src/Components/Shared/Header/Index.jsx
@@ -6,8 +6,8 @@ function Header() {
     <header>
       <div className={styles.container}>
         <div className={styles.brand}>
-          <img className={styles.logo} src="../assets/images/LOGO-RR-1.svg" alt="logo"></img>
-          <img className={styles.isologo} src="../assets/images/MegaRocket.svg" alt="isologo"></img>
+          <img className={styles.logo} src="../assets/images/LOGO-RR-1.svg" alt="Logo"></img>
+          <img className={styles.isologo} src="../assets/images/MegaRocket.svg" alt="IsoLogo"></img>
         </div>
       </div>
       <Nav />
