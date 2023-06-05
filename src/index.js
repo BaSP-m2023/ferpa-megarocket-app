622afa23142f26040f30142c5dabd239b978f245
SantiH97 <santiago.herrera1224@gmail.com>
2023-06-05 19:06:09 -0300

    Set-up/MR-78/Try again

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import Layout from './Components/Layout';
 import './index.css';
-import Header from './Components/Shared/Header/Index';
-import Footer from './Components/Shared/Footer/Index';
+import Header from './Components/Shared/Header/index';
+import Footer from './Components/Shared/Footer/index';
 
 ReactDOM.render(
   <React.StrictMode>
