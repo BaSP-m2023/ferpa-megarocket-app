ba8d906e1df668b0626a7e565d42838ca54b0480
Eugenio Weskamp <e.weskamp8@gmail.com>
2023-06-10 18:44:25 -0300

    MR-91: changes in thunks.js

--- src/redux/superadmins/thunks.js
+++ src/redux/superadmins/thunks.js
@@ -2,7 +2,6 @@ import { getSuperadminsPending, getSuperadminsError, getSuperadminsSuccess } fro
 
 export const getSuperadmins = async (dispatch) => {
   dispatch(getSuperadminsPending());
-  console.log('hi');
   try {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/super-admins/`);
     const data = await response.json();
