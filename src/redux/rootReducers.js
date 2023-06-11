9c9e22696e4a57339c7abac9cac1c8a34efbdacc
Juan Sobral <JuanignacioSobral@gmail.com>
2023-06-11 20:55:14 -0300

    MR-89: First commit

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -3,12 +3,14 @@ import { trainersReducer } from './trainers/reducer';
 import { adminsReducers } from './admins/reducer';
 import activitiesReducer from './activities/reducer';
 import { classReducer } from './classes/reducer';
+import membersReducer from './members/reducer';
 
 const reducers = combineReducers({
   trainers: trainersReducer,
   admins: adminsReducers,
   activities: activitiesReducer,
-  classes: classReducer
+  classes: classReducer,
+  members: membersReducer
 });
 
 export default reducers;
