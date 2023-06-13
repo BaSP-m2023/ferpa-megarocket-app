c6d162a5553894cda0c9726d080f79e344806436
Juan Sobral <JuanignacioSobral@gmail.com>
2023-06-12 21:31:55 -0300

    MR-89: deleted commented code and adjusted CSS

--- src/Components/Members/index.jsx
+++ src/Components/Members/index.jsx
@@ -25,23 +25,13 @@ function Members() {
     }
   };
 
-  // const getMembers = async () => {
-  //   try {
-  //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members`);
-  //     const data = await response.json();
-  //     setMembers(data.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   useEffect(() => {
     getMembers(dispatch);
   }, [dispatch]);
 
   if (isPending) {
     return (
-      <div className={styles.container + ' ' + styles.whiteLetters}>
+      <div className={`${styles.container} ${styles.whiteLetters}`}>
         <div className={styles.list}>
           <div className={styles.header}>
             <h2>Members</h2>
@@ -56,76 +46,78 @@ function Members() {
   }
 
   return (
-    <section className={styles.container + ' ' + styles.whiteLetters}>
-      <div className={styles.header}>
-        <h2>Members</h2>
-        <Link to={'/members/create'}>
-          <Button text={'Create new member'} variant={'add'} />
-        </Link>
-      </div>
-      {error !== '' ? (
-        <div>
-          <p className={styles.whiteLetters}>{error}</p>
+    <section className={`${styles.container} ${styles.whiteLetters}`}>
+      <div className={styles.purpleBack}>
+        <div className={styles.header}>
+          <h2>Members</h2>
+          <Link to={'/members/create'}>
+            <Button text={'Create new member'} variant={'add'} />
+          </Link>
         </div>
-      ) : (
-        <table className={styles.list}>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Surname</th>
-              <th>DNI</th>
-              <th>Email</th>
-              <th>Phone</th>
-            </tr>
-            {data.map((item) => {
-              return (
-                <tr key={item._id}>
-                  <td>{item.firstName}</td>
-                  <td>{item.lastName}</td>
-                  <td>{item.dni}</td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                  <td></td>
-                  <td>
-                    <Link to={`members/edit/${item._id}`}>
-                      <Button variant={'edit'} />
-                    </Link>
-                  </td>
-                  <Modal
-                    onClose={() => setShowModal(false)}
-                    isOpen={showModal}
-                    title={`Are you sure you want to delete this member?`}
-                    warning={true}
-                  >
-                    <Button
-                      text={'Delete'}
-                      variant={'delete'}
-                      clickAction={() => {
-                        deleteMember(memberId);
-                        setShowModal(false);
-                      }}
-                    />
-                    <Button
-                      text={'Cancel'}
-                      variant={'white'}
-                      clickAction={() => setShowModal(false)}
-                    />
-                  </Modal>
-                  <td>
-                    <Button
-                      variant={'deleteIcon'}
-                      clickAction={() => {
-                        setShowModal(true);
-                        setMemberId(item._id);
-                      }}
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
+        {error !== '' ? (
+          <div>
+            <p className={styles.whiteLetters}>{error}</p>
+          </div>
+        ) : (
+          <table className={styles.list}>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>DNI</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+              {data.map((item) => {
+                return (
+                  <tr key={item._id}>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.dni}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td></td>
+                    <td>
+                      <Link to={`members/edit/${item._id}`}>
+                        <Button variant={'edit'} />
+                      </Link>
+                    </td>
+                    <Modal
+                      onClose={() => setShowModal(false)}
+                      isOpen={showModal}
+                      title={`Are you sure you want to delete this member?`}
+                      warning={true}
+                    >
+                      <Button
+                        text={'Delete'}
+                        variant={'delete'}
+                        clickAction={() => {
+                          deleteMember(memberId);
+                          setShowModal(false);
+                        }}
+                      />
+                      <Button
+                        text={'Cancel'}
+                        variant={'white'}
+                        clickAction={() => setShowModal(false)}
+                      />
+                    </Modal>
+                    <td>
+                      <Button
+                        variant={'deleteIcon'}
+                        clickAction={() => {
+                          setShowModal(true);
+                          setMemberId(item._id);
+                        }}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
     </section>
   );
 }
