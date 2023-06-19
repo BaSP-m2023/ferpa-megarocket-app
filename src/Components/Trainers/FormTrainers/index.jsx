f83f2fc88db2fca9cdc3b52802554cdc2389845a
Matias Sauvanet <matiassauvanet@gmail.com>
2023-06-18 22:17:18 -0300

    MR-108: added validations and hook form logic

--- src/Components/Trainers/FormTrainers/index.jsx
+++ src/Components/Trainers/FormTrainers/index.jsx
@@ -3,14 +3,36 @@ import { useState, useEffect } from 'react';
 import { Input } from '../../Shared/Inputs';
 import { sendTrainer, putTrainer } from '../../../redux/trainers/thunks';
 import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
 import React from 'react';
 import Button from '../../Shared/Button';
 import Modal from '../../Shared/Modal';
 import styles from './form.module.css';
 
 const TrainerAddForm = () => {
-  const [successAddModal, setSuccessAddModal] = useState(false);
-  const [successEditModal, setSuccessEditModal] = useState(false);
+  // const [focusPass, setFocusPass] = useState(false);
+  const [errorModal, setErrorModal] = useState(false);
+  const [successModal, setSuccessModal] = useState(false);
+  const { id } = useParams();
+  const { trainers, success, error, formError, message } = useSelector((state) => state.trainers);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const schema = Joi.object({
+    firstName: Joi.string().required().min(3).max(16),
+    lastName: Joi.string().required().min(3).max(16),
+    // dni: Joi.string().required().pattern(new RegExp('^[0-9]+$')).min(7).max(10),
+    // phone: Joi.string().required().pattern(new RegExp('^[0-9]+$')).min(7).max(15),
+    email: Joi.string().required().pattern(new RegExp('^\\S+@\\S+\\.\\S+$')),
+    city: Joi.string().required().min(3).max(20),
+    password: Joi.string()
+      .required()
+      .pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'))
+    // salary: Joi.string().required().min(0)
+  });
+
   const [inputs, setInputs] = useState({
     firstName: '',
     lastName: '',
@@ -21,10 +43,14 @@ const TrainerAddForm = () => {
     password: '',
     salary: ''
   });
-  const { id } = useParams();
-  const { trainers, success, error, formError } = useSelector((state) => state.trainers);
-  const dispatch = useDispatch();
-  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm({ mode: 'onChange', resolver: joiResolver(schema), defaultValues: { inputs } });
+
   useEffect(() => {
     if (id) {
       const trainer = trainers.find((trainer) => trainer._id === id);
@@ -42,101 +68,109 @@ const TrainerAddForm = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [success, formError]);
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
+  useEffect(() => {
+    reset(inputs);
+  }, [inputs, reset]);
 
   const handleModalError = () => {
     setTimeout(() => {
-      setSuccessEditModal(!successEditModal);
+      setSuccessModal(!successModal);
     }, 2000);
   };
 
   const handleModalSuccess = () => {
     setTimeout(() => {
       history.push('/trainers');
-      setSuccessAddModal(!successAddModal);
+      setErrorModal(!errorModal);
     }, 2000);
   };
 
-  const onSubmitAdd = (e) => {
-    e.preventDefault();
-    setSuccessAddModal(!successAddModal);
-    sendTrainer(dispatch, inputs);
+  const onSubmitAdd = (data) => {
+    setErrorModal(!errorModal);
+    sendTrainer(dispatch, data);
   };
   const onSubmitEdit = (e) => {
-    e.preventDefault();
-    setSuccessEditModal(!successEditModal);
+    setSuccessModal(!successModal);
     putTrainer(dispatch, id, inputs);
   };
   return (
     <div className={styles.formContainer}>
       <div className={styles.fromBackground}>
-        <form className={styles.form} onSubmit={id ? onSubmitEdit : onSubmitAdd}>
+        <form className={styles.form} onSubmit={handleSubmit(id ? onSubmitEdit : onSubmitAdd)}>
           <div className={styles.field}>
             <Input
+              register={register}
               labelText={'Name'}
               nameValue={'firstName'}
               placeholder={'First Name'}
-              value={inputs.firstName}
-              onChangeInput={handleChange}
+              error={errors.firstName?.message}
+              // value={inputs.firstName}
             />
             <Input
+              register={register}
               labelText={'LastName'}
               nameValue={'lastName'}
               placeholder={'Last Name'}
-              value={inputs.lastName}
-              onChangeInput={handleChange}
+              error={errors.lastName?.message}
+              // value={inputs.lastName}
             />
             <Input
+              register={register}
               labelText={'DNI'}
               nameValue={'dni'}
               placeholder={'DNI'}
-              value={inputs.dni}
-              onChangeInput={handleChange}
+              error={errors.dni?.message}
+              // value={inputs.dni}
             />
             <Input
+              register={register}
               labelText={'Phone'}
               nameValue={'phone'}
               placeholder={'Phone'}
-              value={inputs.phone}
-              onChangeInput={handleChange}
+              error={errors.phone?.message}
+              // value={inputs.phone}
             />
             <Input
+              register={register}
               labelText={'Email'}
               nameValue={'email'}
               placeholder={'Email'}
-              value={inputs.email}
-              onChangeInput={handleChange}
+              error={errors.email?.message}
+              // value={inputs.email}
             />
             <Input
+              register={register}
               labelText={'City'}
               nameValue={'city'}
               placeholder={'City'}
-              value={inputs.city}
-              onChangeInput={handleChange}
+              error={errors.city?.message}
+              // value={inputs.city}
             />
             <Input
+              register={register}
               labelText={'Salary'}
               nameValue={'salary'}
               placeholder={'Salary'}
-              value={inputs.salary}
-              onChangeInput={handleChange}
+              error={errors.salary?.message}
+              // value={inputs.salary}
             />
             <Input
+              register={register}
               labelText={'Password'}
               nameValue={'password'}
               type={'password'}
               placeholder={'Password'}
-              value={inputs.password}
-              onChangeInput={handleChange}
+              // onFocus={() => setFocusPass(true)}
+              // onBlur={() => setFocusPass(false)}
+              error={errors.password?.message}
+              // value={inputs.password}
             />
-            <p className={styles.passwordAlert}>
-              * The password must have uppercase letters, lowercase letters, number and at least 8
-              characters
-            </p>
+            {/* {focusPass && (
+              <p className={styles.passwordAlert}>
+                * The password must have uppercase letters, lowercase letters, number and at least 8
+                characters
+              </p> */}
+            {/* )} */}
           </div>
           <div className={styles.buttons}>
             <Link to={'/trainers'}>
@@ -145,16 +179,16 @@ const TrainerAddForm = () => {
             <Button text={id ? 'Edit' : 'Add'} variant={'add'} submitting />
           </div>
           <Modal
-            success
-            isOpen={successAddModal}
-            onClose={() => setSuccessAddModal(!successAddModal)}
+            error
+            isOpen={errorModal}
+            onClose={() => setErrorModal(!errorModal)}
             title={error}
           ></Modal>
           <Modal
             success
-            isOpen={successEditModal}
-            onClose={() => setSuccessEditModal(!successEditModal)}
-            title={error}
+            isOpen={successModal}
+            onClose={() => setSuccessModal(!successModal)}
+            title={message}
           ></Modal>
         </form>
       </div>
