2ebfeff1911c5ca4dc3bfebdb9ed42c984abae7f
Juan Sobral <JuanignacioSobral@gmail.com>
2023-06-20 08:02:14 -0300

    MR-112: Generated activities list, homepage, profile page and general CSS. Connected profile with edit profile, but page seems to crash due to change on the shared inputs file

--- src/Components/MembersHome/Activities/index.jsx
+++ src/Components/MembersHome/Activities/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styles from './activities.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getActivities } from '../../../redux/activities/thunks';
 
 const Activities = () => {
-  return <div>Activities</div>;
+  const { data } = useSelector((state) => state.activities);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getActivities(dispatch);
+  }, []);
+
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.header}>Activities</h2>
+      <div className={styles.body}>
+        {data.map((activity) => {
+          return (
+            <div className={styles.activity} key={activity._id}>
+              <div className={styles.activityImage}>
+                <img
+                  src="https://st.depositphotos.com/1708346/1404/i/450/depositphotos_14043607-stock-photo-college-gym-49ers-football-team.jpg"
+                  alt="Gym Image"
+                />
+              </div>
+              <div className={styles.activityData}>
+                <h3 className={styles.activityName}>{activity.name}</h3>
+                <p className={styles.activityDescription}>{activity.description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Activities;
--- src/Components/MembersHome/Profile/index.jsx
+++ src/Components/MembersHome/Profile/index.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import styles from './profile.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getMembers } from '../../../redux/members/thunks';
+import Loader from '../../Shared/Loader';
+import Button from '../../Shared/Button';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { data, isPending } = useSelector((state) => state.members);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMembers());
+  }, []);
+
+  if (isPending) {
+    return <Loader />;
+  }
+
+  const year = data[0].birthDay.slice(0, 4);
+  const month = data[0].birthDay.slice(5, 7);
+  const day = data[0].birthDay.slice(8, 10);
+  const fixedBirthday = day + '/' + month + '/' + year;
+
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.header}>Your profile</h2>
+      <div className={styles.body}>
+        <div className={styles.dataWraper}>
+          <div className={styles.userData}>
+            <p className={styles.infoTag}>Name:</p>
+            <p className={styles.userInfo}>{data[0].firstName}</p>
+            <p className={styles.infoTag}>Last Name:</p>
+            <p className={styles.userInfo}>{data[0].lastName}</p>
+            <p className={styles.infoTag}>DNI:</p>
+            <p className={styles.userInfo}>{data[0].dni}</p>
+            <p className={styles.infoTag}>Birthday:</p>
+            <p className={styles.userInfo}>{fixedBirthday}</p>
+          </div>
+          <div className={styles.userData}>
+            <p className={styles.infoTag}>Phone:</p>
+            <p className={styles.userInfo}>{data[0].phone}</p>
+            <p className={styles.infoTag}>Email:</p>
+            <p className={styles.userInfo}>{data[0].email}</p>
+            <p className={styles.infoTag}>City:</p>
+            <p className={styles.userInfo}>{data[0].city}</p>
+            <p className={styles.infoTag}>Postal Code:</p>
+            <p className={styles.userInfo}>{data[0].postalCode}</p>
+          </div>
+          <div>
+            <Link to={`/members/home/edit/${data[0]._id}`}>
+              <Button text={'Edit your profile!'} variant={'add'} />
+            </Link>
+          </div>
+        </div>
+        <div className={styles.membershipData}>
+          <div className={styles.membershipUnit}>
+            <p className={styles.infoTag}>Current membership:</p>
+            <p className={styles.membershipType}>{data[0].membership}</p>
+          </div>
+          {data[0].isActive ? (
+            <div className={`${styles.membershipUnit} ${styles.activeYes}`}>
+              You are an active user
+            </div>
+          ) : (
+            <div className={`${styles.membershipUnit} ${styles.activeYes}`}>
+              You are not an active user
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
