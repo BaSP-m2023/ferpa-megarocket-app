5386c787637400966821758bf1fd3dd424ebc267
Juan Sobral <JuanignacioSobral@gmail.com>
2023-06-21 21:40:37 -0300

    MR-112: adjusted CSS to fit common style agreements

--- src/Components/MembersHome/Profile/index.jsx
+++ src/Components/MembersHome/Profile/index.jsx
@@ -27,49 +27,59 @@ const Profile = () => {
   return (
     <section className={styles.container}>
       <h2 className={styles.header}>Your profile</h2>
-      <div className={styles.body}>
-        <div className={styles.dataWraper}>
-          <div className={styles.userData}>
+      <div className={styles.dataWraper}>
+        <div className={styles.userData}>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Name:</p>
             <p className={styles.userInfo}>{data[0].firstName}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Last Name:</p>
             <p className={styles.userInfo}>{data[0].lastName}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>DNI:</p>
             <p className={styles.userInfo}>{data[0].dni}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Birthday:</p>
             <p className={styles.userInfo}>{fixedBirthday}</p>
           </div>
-          <div className={styles.userData}>
+          <div className={styles.inputDuo}>
+            <p className={styles.infoTag}>Current membership:</p>
+            <p className={styles.userInfo}>{data[0].membership}</p>
+          </div>
+        </div>
+        <div className={styles.userData}>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Phone:</p>
             <p className={styles.userInfo}>{data[0].phone}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Email:</p>
             <p className={styles.userInfo}>{data[0].email}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>City:</p>
             <p className={styles.userInfo}>{data[0].city}</p>
+          </div>
+          <div className={styles.inputDuo}>
             <p className={styles.infoTag}>Postal Code:</p>
             <p className={styles.userInfo}>{data[0].postalCode}</p>
           </div>
-          <div>
-            <Link to={`/members/home/edit/${data[0]._id}`}>
-              <Button text={'Edit your profile!'} variant={'add'} />
-            </Link>
+          <div className={styles.inputDuo}>
+            {data[0].isActive ? (
+              <div className={styles.userInfo}>You are an active user</div>
+            ) : (
+              <div className={styles.userInfo}>You are not an active user</div>
+            )}
           </div>
         </div>
-        <div className={styles.membershipData}>
-          <div className={styles.membershipUnit}>
-            <p className={styles.infoTag}>Current membership:</p>
-            <p className={styles.membershipType}>{data[0].membership}</p>
-          </div>
-          {data[0].isActive ? (
-            <div className={`${styles.membershipUnit} ${styles.activeYes}`}>
-              You are an active user
-            </div>
-          ) : (
-            <div className={`${styles.membershipUnit} ${styles.activeYes}`}>
-              You are not an active user
-            </div>
-          )}
-        </div>
+      </div>
+      <div className={styles.editButton}>
+        <Link to={`/members/home/edit/${data[0]._id}`}>
+          <Button text={'Edit your profile!'} variant={'add'} />
+        </Link>
       </div>
     </section>
   );
