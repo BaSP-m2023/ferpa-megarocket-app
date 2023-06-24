238535aefcc3181a2464c41a448fa08a302f5646
Cristian Zufiaurre <cjzufi@gmail.com>
2023-06-24 02:47:45 -0300

    MR-117: finishing details and styles

--- src/Components/AdminsHome/Reports/index.jsx
+++ src/Components/AdminsHome/Reports/index.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
 import styles from './reports.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTrainers } from 'redux/trainers/thunks';
 import { getMembers } from 'redux/members/thunks';
 import { getClasses } from 'redux/classes/thunks';
 import { getSubscriptions } from 'redux/subscriptions/thunks';
+import Loader from 'Components/Shared/Loader';
 
 const Reports = () => {
-  const { trainers } = useSelector((state) => state.trainers);
   const { classes } = useSelector((state) => state.classes);
   const { subs } = useSelector((state) => state.subscriptions);
   const members = useSelector((state) => state.members.data);
+  const classesLoading = useSelector((state) => state.classes.isLoading);
+  const subsLoading = useSelector((state) => state.subscriptions.isPending);
+  const membersLoading = useSelector((state) => state.members.isPending);
   const membersActive = members.filter((member) => member.isActive === true).length;
-  const trainersActive = trainers.filter((trainer) => trainer.isActive === true).length;
-  const trainersSalary = trainers.reduce((acc, trainer) => acc + trainer.salary, 0);
+  const membershipsActive = members.filter((member) => member.isMembershipActive === true).length;
   const dispatch = useDispatch();
 
   const countMemberships = (members, membership) => {
@@ -48,48 +49,59 @@ const Reports = () => {
   const totalMembership = countMemberships(members, 'membership');
 
   useEffect(() => {
-    getTrainers(dispatch);
     dispatch(getMembers());
     dispatch(getClasses());
     getSubscriptions(dispatch);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(classes.find((clas) => clas._id === mostPopularClass[0].value));
+
+  if (classesLoading || membersLoading || subsLoading) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <Loader />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>Reports</h1>
         <section className={styles.repcontainer}>
-          <h2 className={styles.subtitle}>Trainers</h2>
-          <div className={styles.reports}>
-            <p>
-              Active trainers: <span>{trainersActive}</span>
-            </p>
-            <p>
-              Total salaries of the trainers: <span>$ {trainersSalary}</span>
-            </p>
-            <p>
-              Average trainer salary <span>$ {(trainersSalary / trainers.length).toFixed(2)}</span>
-            </p>
-          </div>
+          <h2 className={styles.subtitle}>Members</h2>
+          {members.length > 0 ? (
+            <div className={styles.reports}>
+              <p>
+                Total members: <span>{members.length}</span>
+              </p>
+              <p>
+                Members active: <span>{membersActive}</span>
+              </p>
+              <p>
+                Memberships active: <span>{membershipsActive}</span>
+              </p>
+            </div>
+          ) : (
+            <p className={styles.subtitle}>There are no registered members</p>
+          )}
         </section>
         <section className={styles.repcontainer}>
-          <h2 className={styles.subtitle}>Members Reports </h2>
-          <div className={styles.reports}>
-            <p>
-              Members: <span>{members.length}</span>
-            </p>
-            <p>
-              Members Active: <span>{membersActive}</span>
-            </p>
-            <p></p>
-          </div>
-          <h3>Only Classes = {totalMembership['Only Classes'] || 0}</h3>
-          <h3>Classic = {totalMembership['Classic'] || 0}</h3>
-          <h3>Black = {totalMembership['Black'] || 0}</h3>
+          <h2 className={styles.subtitle}>Memberships</h2>
+          {members.length > 0 ? (
+            <div className={styles.reports}>
+              <p>Only Classes: {totalMembership['Only Classes'] || 0}</p>
+              <p>Classic: {totalMembership['Classic'] || 0}</p>
+              <p>Black: {totalMembership['Black'] || 0}</p>
+            </div>
+          ) : (
+            <p className={styles.subtitle}>There are no registered memberships</p>
+          )}
         </section>
         <section className={styles.repcontainer}>
-          <h2 className={styles.subtitle}>Popular Classes</h2>
+          <h2 className={styles.subtitle}>
+            {mostPopularClass.length === 1 ? 'Popular class' : 'Popular classes'}
+          </h2>
           <div className={styles.popularContainer}>
             {subs.length > 1 && classes ? (
               mostPopularClass.map((item, index) => (
@@ -107,7 +119,7 @@ const Reports = () => {
                 </div>
               ))
             ) : (
-              <p></p>
+              <p className={styles.subtitle}>There are no classes with subscribed members</p>
             )}
           </div>
         </section>
