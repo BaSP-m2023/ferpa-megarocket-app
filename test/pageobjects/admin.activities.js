2ff094b3fa5a7434b04b1ec49882864afe173155
Milagros <cerromilagros1@gmail.com>
2023-06-27 13:14:56 -0300

    MR-133: admins pageObject and e2e of activities CRUD

--- test/pageobjects/admin.activities.js
+++ test/pageobjects/admin.activities.js
@@ -1,2 +1,157 @@
-class ActivitiesPage {}
+/* eslint-disable no-undef */
+class ActivitiesPage {
+  //buttoms and modals
+  get editBtn() {
+    return $("[data-testid='edit-btn']");
+  }
+  get confirmEditBtn() {
+    return $("[data-testid='confirm-edit-btn']");
+  }
+  get cancelBtn() {
+    return $("[data-testid='cancel-btn']");
+  }
+  get successModal() {
+    return $("[data-testid='success-modal']");
+  }
+
+  //sheared component
+  get header() {
+    return $("[data-testid='header-container']");
+  }
+  get logo() {
+    return $("[data-testid='header-logo']");
+  }
+  get navbar() {
+    return $("[data-testid='nav-container']");
+  }
+  get navProfile() {
+    return $("[data-testid='admins-profile']");
+  }
+  get navReports() {
+    return $("[data-testid='admins-reports']");
+  }
+  get navActivities() {
+    return $("[data-testid='admins-activities']");
+  }
+  get navClasses() {
+    return $("[data-testid='admins-classes']");
+  }
+  get navMembers() {
+    return $("[data-testid='admins-members']");
+  }
+  get navSubs() {
+    return $("[data-testid='admins-subs']");
+  }
+  get navTrainers() {
+    return $("[data-testid='admins-trainers']");
+  }
+  get footer() {
+    return $("[data-testid='footer-container']");
+  }
+
+  //form
+  get formAdmin() {
+    return $("[data-testid='admin-editform']");
+  }
+  get formAdminFirstChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(2) input");
+  }
+  get formAdminSecondChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(3) input");
+  }
+  get formAdminThirdChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(4) input");
+  }
+  get formAdminFourthChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(5) input");
+  }
+  get formAdminFifthChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(6) input");
+  }
+  get formAdminSixthChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(7) input");
+  }
+  get formAdminSeventhChild() {
+    return $("[data-testid='admin-editform'] div:nth-child(8) input");
+  }
+
+  get formFirstLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(2) label");
+  }
+  get formSecondLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(3) label");
+  }
+  get formThirdLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(4) label");
+  }
+  get formFourthLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(5) label");
+  }
+  get formFifthLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(6) label");
+  }
+  get formSixthLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(7) label");
+  }
+  get formSeventhLabel() {
+    return $("[data-testid='admin-editform'] div:nth-child(8) label");
+  }
+
+  //activities
+  get addBtn() {
+    return $("[data-testid='add-btn']");
+  }
+  get pencilEditBtn() {
+    return $("[data-testid='edit-btn']");
+  }
+  get deleteBtn() {
+    return $("[data-testid='delete-btn']");
+  }
+  get addAndEditBtn() {
+    return $("[data-testid='delete-btn']");
+  }
+  get formActivities() {
+    return $("[data-testid='activity-add-edit-container']");
+  }
+  get addFormActivitiesFirst() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(2) imput");
+  }
+  get formActivitieFirstLabel() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(2) label");
+  }
+  get addFormActivitiesSecond() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(3) imput");
+  }
+  get formActivitieSecondLabel() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(3) label");
+  }
+  get addFormActivitiesThird() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(4) imput");
+  }
+  get formActivitieThirdLabel() {
+    return $("[data-testid='activity-add-edit-container'] div:nth-child(4) label");
+  }
+
+  async editAdminBtnClick() {
+    await this.editBtn.click();
+  }
+  async editBtnClick() {
+    await this.confirmEditBtn.click();
+  }
+  async cancelBtnClick() {
+    await this.cancelBtn.click();
+  }
+  async editActivitiesBtn() {
+    await this.addBtn.click();
+  }
+  async AddActivitiesBtn() {
+    await this.addBtn.click();
+  }
+  async addAndEditBtnClick() {
+    await this.addAndEditBtn.click();
+  }
+  async deleteActivitiesBtnClick() {
+    await this.deleteBtn.click();
+  }
+}
 module.exports = new ActivitiesPage();
--- test/specs/admin.activities.e2e.js
+++ test/specs/admin.activities.e2e.js
@@ -1,8 +1,140 @@
 /* eslint-disable no-undef */
 const ActivitiesPage = require('../../test/pageobjects/admin.activities');
-describe('Happy path Member entity.', () => {
+describe('Happy path Admin entity.', () => {
   beforeAll('Open browser', () => {
     browser.setWindowSize(1920, 1080);
     browser.url('https://ferpa-megarocket-app.vercel.app/admins/home/profile');
   });
+  it('Verify correct display of Admin profile page.', async () => {
+    await expect(ActivitiesPage.header).toBeDisplayed();
+    await expect(ActivitiesPage.logo).toBeDisplayed();
+    await expect(ActivitiesPage.navbar).toBeDisplayed();
+
+    await expect(ActivitiesPage.navProfile).toBeDisplayed();
+    await expect(ActivitiesPage.navReports).toBeDisplayed();
+    await expect(ActivitiesPage.navActivities).toBeDisplayed();
+    await expect(ActivitiesPage.navClasses).toBeDisplayed();
+    await expect(ActivitiesPage.navMembers).toBeDisplayed();
+    await expect(ActivitiesPage.navSubs).toBeDisplayed();
+    await expect(ActivitiesPage.navTrainers).toBeDisplayed();
+    await browser.pause(1000);
+    await ActivitiesPage.footer.scrollIntoView();
+    await expect(ActivitiesPage.footer).toBeDisplayed();
+  });
+
+  it('Verify open and close the form.', async () => {
+    await ActivitiesPage.editAdminBtnClick();
+    await browser.pause(2000);
+    await expect(ActivitiesPage.formAdmin).toBeDisplayed();
+    await ActivitiesPage.cancelBtnClick();
+  });
+
+  it('Verify edit profile form.', async () => {
+    await browser.pause(2000);
+    await ActivitiesPage.editAdminBtnClick();
+    await browser.pause(2000);
+    await expect(ActivitiesPage.formAdmin).toBeDisplayed();
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminFirstChild.waitForDisplayed();
+    await expect(ActivitiesPage.formFirstLabel).toHaveTextContaining('First Name');
+    await ActivitiesPage.formAdminFirstChild.setValue('Mili');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminSecondChild.waitForDisplayed();
+    await expect(ActivitiesPage.formSecondLabel).toHaveTextContaining('Last Name');
+    await ActivitiesPage.formAdminSecondChild.setValue('Cerro');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminThirdChild.waitForDisplayed();
+    await expect(ActivitiesPage.formThirdLabel).toHaveTextContaining('DNI');
+    await ActivitiesPage.formAdminThirdChild.setValue('42400400');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminFourthChild.waitForDisplayed();
+    await expect(ActivitiesPage.formFourthLabel).toHaveTextContaining('Phone');
+    await ActivitiesPage.formAdminFourthChild.setValue('3416167612');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminFifthChild.waitForDisplayed();
+    await expect(ActivitiesPage.formFifthLabel).toHaveTextContaining('Email');
+    await ActivitiesPage.formAdminFifthChild.setValue('c@m.com');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminSixthChild.waitForDisplayed();
+    await expect(ActivitiesPage.formSixthLabel).toHaveTextContaining('City');
+    await ActivitiesPage.formAdminSixthChild.setValue('Rosario');
+
+    await browser.pause(1000);
+    await ActivitiesPage.formAdminSeventhChild.waitForDisplayed();
+    await expect(ActivitiesPage.formSeventhLabel).toHaveTextContaining('Password');
+    await ActivitiesPage.formAdminSeventhChild.setValue('Mili1234&');
+
+    await browser.pause(1000);
+    await ActivitiesPage.editBtnClick();
+    await browser.pause(2000);
+    await ActivitiesPage.successModal.waitForDisplayed();
+  });
+
+  it('Verify correct flow and display of the navigation bar.', async () => {
+    await browser.pause(3000);
+    await expect(ActivitiesPage.navbar).toBeDisplayed();
+    await expect(ActivitiesPage.navProfile).toBeDisplayed();
+
+    await expect(ActivitiesPage.navReports).toBeDisplayed();
+    await ActivitiesPage.navReports.click();
+    await browser.pause(1000);
+    await expect(browser).toHaveUrl('https://ferpa-megarocket-app.vercel.app/admins/home/reports');
+    await browser.pause(1000);
+  });
+
+  it('Verify correct CRUD of activities.', async () => {
+    await expect(ActivitiesPage.navActivities).toBeDisplayed();
+    await ActivitiesPage.navActivities.click();
+    await browser.pause(1000);
+    await expect(browser).toHaveUrl(
+      'https://ferpa-megarocket-app.vercel.app/admins/home/activities'
+    );
+    await expect(ActivitiesPage.addBtn).toBeDisplayed();
+    await ActivitiesPage.editActivitiesBtn();
+
+    await expect(ActivitiesPage.formActivities).toBeDisplayed();
+    await browser.pause(1000);
+    //await ActivitiesPage.addFormActivitiesFirst.waitForDisplayed();
+    // await expect(ActivitiesPage.formActivitieFirstLabel).toHaveTextContaining('Name');
+    // await ActivitiesPage.addFormActivitiesFirst.setValue('Contemporaneo');
+
+    //await ActivitiesPage.addFormActivitiesSecond.waitForDisplayed();
+    // await expect(ActivitiesPage.formActivitieSecondLabel).toHaveTextContaining('Description');
+    // await ActivitiesPage.addFormActivitiesSecond.setValue(
+    //   'The Contemporary Age is the name by which the historical period'
+    // );
+
+    await ActivitiesPage.addFormActivitiesThird.waitForDisplayed();
+    await expect(ActivitiesPage.formActivitieThirdLabel).toHaveTextContaining('Is Active?');
+    await ActivitiesPage.addFormActivitiesThird.click();
+
+    await ActivitiesPage.addAndEditBtnClick();
+    await ActivitiesPage.successModal.waitForDisplayed();
+  });
+
+  // it('Verify edit activity from the form.', async () => {
+  //   await expect(ActivitiesPage.formActivities).toBeDisplayed();
+  //   await browser.pause(1000);
+  //   await expect(ActivitiesPage.formActivitieFirstLabel).toHaveTextContaining('Name');
+  //   await expect(ActivitiesPage.formActivitieSecondLabel).toHaveTextContaining('Description');
+  //   await expect(ActivitiesPage.formActivitieThirdLabel).toHaveTextContaining('Is Active?');
+  //   await ActivitiesPage.addAndEditBtnClick();
+  //   await ActivitiesPage.successModal.waitForDisplayed();
+  // });
+
+  // it('Verify delete activity from the form.', async () => {
+  //   await expect(ActivitiesPage.formActivities).toBeDisplayed();
+  //   await browser.pause(1000);
+  //   await expect(ActivitiesPage.formActivitieFirstLabel).toHaveTextContaining('Name');
+  //   await expect(ActivitiesPage.formActivitieSecondLabel).toHaveTextContaining('Description');
+  //   await expect(ActivitiesPage.formActivitieThirdLabel).toHaveTextContaining('Is Active?');
+  //   await ActivitiesPage.deleteActivitiesBtnClick();
+  //   await ActivitiesPage.successModal.waitForDisplayed();
+  // });
 });
